368812e2ef6b201bbc92e87f2fab1ecdb6c69aeb
Сексуально <@DESKTOP-MQ5E1MJ>
2022-03-29 09:04:03 +0300

    Добавьте файлы проекта.

---
+++ 6/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+namespace _6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.monthpay = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Цена покупки";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Первоначальный платеж";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Срок (лет)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Процентная ставка (%)";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(286, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(286, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(139, 23);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(286, 138);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(139, 23);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // monthpay
+            // 
+            this.monthpay.Location = new System.Drawing.Point(183, 193);
+            this.monthpay.Multiline = true;
+            this.monthpay.Name = "monthpay";
+            this.monthpay.ReadOnly = true;
+            this.monthpay.Size = new System.Drawing.Size(139, 26);
+            this.monthpay.TabIndex = 7;
+            this.monthpay.TextChanged += new System.EventHandler(this.monthpay_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 55);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Вычислить ежемесячный платеж";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 55);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Построить схему платежей";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1 год",
+            "2 года",
+            "3 года"});
+            this.comboBox1.Location = new System.Drawing.Point(286, 101);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(139, 23);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Enter += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 324);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(410, 301);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Column1.HeaderText = "Период";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 74;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Column2.HeaderText = "Основной платеж";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 120;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Column3.HeaderText = "Платеж по %";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 87;
+            // 
+            // Column4
+            // 
+            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Column4.HeaderText = "income";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 72;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(183, 255);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(94, 19);
+            this.radioButton1.TabIndex = 12;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ежемесячно";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged_1);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(183, 280);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(109, 19);
+            this.radioButton2.TabIndex = 13;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ежеквартально";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 637);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthpay);
+            this.Name = "Form1";
+            this.Text = "Расчет платежей по кредиту";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox monthpay;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+
+        public EventHandler label1_Click { get; private set; }
+
+        private EventHandler form1_Load;
+
+        public EventHandler GetForm1_Load()
+        {
+            return form1_Load;
+        }
+
+        private void SetForm1_Load(EventHandler value)
+        {
+            form1_Load = value;
+        }
+    }
+}
+
---
+++ 6/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {}
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+                e.Handled = true;
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+        private void radioButton2_CheckedChanged_1(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+        }
+        private void radioButton1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void monthpay_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        public void Button1_Click(object sender, EventArgs e)     //monthpay
+        {
+            if ((textBox1.Text.Length == 0) || (textBox2.Text.Length == 0) || (textBox3.Text.Length == 0))
+                MessageBox.Show("empty");
+            else
+            {
+                int pokupka = int.Parse(textBox1.Text);
+                int per_plat = int.Parse(textBox2.Text);
+                decimal fact_proc = decimal.Parse(textBox3.Text) / 100;
+
+
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    var mp = ((pokupka - per_plat) * fact_proc) / 12;
+                    var mp2 = (pokupka - per_plat) / 12;  
+                    var xx = Convert.ToString(Math.Round(mp+mp2, 2));
+                    monthpay.Text = xx;
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    var mp = ((pokupka - per_plat) + pokupka * fact_proc) / 24;
+                    var mp2 = (pokupka - per_plat) / 24;
+                    var xx = Convert.ToString(Math.Round(mp + mp2, 2));
+                    monthpay.Text = xx;
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    var mp = ((pokupka - per_plat) + pokupka * fact_proc) / 36; 
+                    var mp2 = (pokupka - per_plat) / 36;
+                    var xx = Convert.ToString(Math.Round(mp + mp2, 36));
+                    monthpay.Text = xx;
+                }
+
+            }
+        }
+
+        public void button2_Click(object sender, EventArgs e)//payschema
+        {
+            monthpay.Clear();
+            static decimal div_r(decimal num_one, decimal num_two) //функция делит и округляет
+            {
+            decimal f_div_r = Math.Round(num_one / num_two, 2);
+            return f_div_r;
+            }
+
+            decimal procik, pok, proc_, //процик - общий процент, пок - платеж по телу долга за период
+                    income = 0, inc = 0,
+                    per_plat = Convert.ToDecimal(textBox2.Text),//firstpay
+                    pokupka_ = Convert.ToDecimal(textBox1.Text),//count
+                    fact_proc = Convert.ToDecimal(textBox3.Text) / 100, // x/100
+                    pokupka = pokupka_ - per_plat,//долг банку
+                    proc = fact_proc * pokupka;
+            decimal pok_ = 0, procik_ = 0;
+
+            if (radioButton1.Checked) //ежемесячный платеж
+            {
+                if ((textBox1.Text.Length == 0) || (textBox2.Text.Length == 0) || (textBox3.Text.Length == 0))
+                    MessageBox.Show("empty");//empty textboxes
+
+                else
+                {
+                    dataGridView1.Rows.Clear();
+                    if (comboBox1.SelectedIndex == 0)
+                    {
+                        procik = div_r(proc, 12);//весь процент делим на период
+                        pok = div_r(pokupka, 12);
+                        for (var i = 1; i <= 12; i++)// i - period
+                        {
+                            income += pok + procik;
+                            dataGridView1.Rows.Add(i, pok, procik, income);                                                                                               
+                        }
+                    }
+
+                    else if (comboBox1.SelectedIndex == 1)
+                    {
+                        procik = div_r(proc, 24);//весь процент делим на период
+                        pok = div_r(pokupka, 24);
+                        for (var i = 1; i <= 24; i++)// i - period
+                        {
+                            income += pok + procik;
+                            dataGridView1.Rows.Add(i, pok, procik, income);
+                        }
+                    }
+                    else if (comboBox1.SelectedIndex == 2)
+                    {
+                        procik = div_r(proc, 36);//весь процент делим на период
+                        pok = div_r(pokupka, 36);
+                        for (var i = 1; i <= 36; i++)// i - period
+                        {
+                            income += pok + procik;
+                            dataGridView1.Rows.Add(i, pok, procik, income);
+                        }
+                    }
+                }
+            }
+
+
+
+
+
+
+            if (radioButton2.Checked)//ежеквартально
+            {
+                if ((textBox1.Text.Length == 0) || (textBox2.Text.Length == 0) || (textBox3.Text.Length == 0))
+                    MessageBox.Show("empty"); //chek texboxes
+                else
+                {
+                    dataGridView1.Rows.Clear();
+                    
+
+                    if (comboBox1.SelectedIndex == 0) //chosen period
+                    {
+                        pok = div_r(pokupka, 12);//весь долг делим на период 
+
+                        for (var i = 1; i <= 12; i++)// i - period
+                        {
+                            procik = div_r(proc, 12);
+                            procik_ += procik;
+                            inc = (pok + procik);
+                            income += inc; // сложение с присваиванием
+                            pok_ += pok;
+                           
+
+                            if (i % 3 == 0)
+                            dataGridView1.Rows.Add((i), pok_, procik_, income);   
+                        }
+                    }
+
+                    else if (comboBox1.SelectedIndex == 1)///
+                    {
+                        fact_proc += fact_proc * 2;
+                        pok = div_r(pokupka, 24);//весь долг делим на период 
+
+                        for (var i = 1; i <= 24; i++)// i - period
+                        {
+                            procik = div_r(proc*2, 12);
+                            procik_ += procik;
+                            inc = (pok + procik);
+                            income += inc; // сложение с присваиванием
+                            pok_ += pok;
+
+                            if (i % 3 == 0)
+                                dataGridView1.Rows.Add((i), pok_, procik_, income);
+                        }
+                    }
+
+                    else if (comboBox1.SelectedIndex == 2)
+                    {
+                        procik = div_r(proc*3, 12);
+                        procik_ += procik;
+                        pok = div_r(pokupka, 36);//весь долг делим на период 
+                                             
+                        for (var i = 1; i <= 36; i++)// i - period
+                        {
+                            inc = (pok + procik);
+                            income += inc; // сложение с присваиванием
+                            pok_ += pok;
+
+                            if (i % 3 == 0)
+                                dataGridView1.Rows.Add((i), pok_, procik_, income);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
